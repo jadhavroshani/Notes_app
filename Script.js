c91767f6105316005702cb7d23d92416aa7bf78c
jadhavroshani <roshanijadhav2169@gmail.com>
2021-06-08 23:04:52 +0530

    1st commmit

---
+++ Script.js
@@ -0,0 +1,49 @@
+const addBtn = document.getElementById('add');
+addBtn.addEventListener('click', () => 
+{
+    addNewNote();
+});
+
+function addNewNote(){
+    const note = document.createElement('div');
+    note.classList.add('note')
+
+    note.innerHTML=`
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-clipboard"></i></button>
+                <button class="delete"><i class="fas fa-trash"></i></button>
+            </div>
+            <div class="main hidden">
+
+            </div>
+            
+
+            <textarea style="background-color:rgb(190, 245, 237);"></textarea>
+        </div>
+        `;
+        
+
+const editBtn =note.querySelector(".edit");
+const deleteBtn =note.querySelector(".delete");
+
+const main = note.querySelector(".main");
+const textArea = note.querySelector("textarea");
+
+editBtn.addEventListener("click", ()=>{
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+});
+deleteBtn.addEventListener("click",() =>{
+    note.remove();
+});
+
+textArea.addEventListener("input",(e) =>{
+const {value}=e.target;
+
+
+main.innerHTML=marked(value);
+});
+
+    document.body.appendChild(note);
+}
